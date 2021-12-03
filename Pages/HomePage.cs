7d2f8b537377bb86e6e1034d7cb0aaa2240cbf37
Piotr Kościółek <kosciolek.piotr@gmail.com>
2021-12-03 07:50:57 +0100

    home page fix

---
+++ Pages/HomePage.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeleniumCSharpNetCore.Pages
+{
+    class HomePage : DriverHelper
+    {
+        public void ClickLogin()
+        {
+            IWebElement btnLogin = Driver.FindElement(By.Id("loginLink"));
+            btnLogin.Click();
+        }
+        public bool IsLogOffExist()
+        {
+            IWebElement btnLogoff = Driver.FindElement(By.LinkText("Log off"));
+            return btnLogoff.Displayed;
+        }
+    }
+}
---
+++ Pages/LoginPage.cs
@@ -0,0 +1,26 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SeleniumCSharpNetCore.Pages
+{
+    class LoginPage : DriverHelper
+    {
+        IWebElement txtUserName = Driver.FindElement(By.Id("UserName"));
+        IWebElement txtPassword = Driver.FindElement(By.Id("Password"));
+        IWebElement btnLogin = Driver.FindElement(By.XPath("//input[@value='Log in']"));
+
+        public void EnterUsernameAndPass(string userName, string password)
+        {
+            txtUserName.SendKeys(userName);
+            txtPassword.SendKeys(password);
+        }
+
+        public void ClickLogin()
+        {
+            btnLogin.Click();
+        }
+
+    }
+}
